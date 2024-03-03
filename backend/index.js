f2574a6a0347c6a7a5fc6f9da3b36f4422811c8b
Manav Chawla <chawlamanav428@gmail.com>
2024-03-04 00:21:06 +0530

    done with user routes

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import multer from "multer";
 import helmet from "helmet";
 import path from "path";
 import authroutes from "./routes/auth.js";
+import userroutes from "./routes/user.js";
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
 const __filename = fileURLToPath(import.meta.url);
@@ -32,6 +33,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 app.post("/auth/register", upload.single("picture"), register);
 app.use("/auth", authroutes);
+app.use("/users", userroutes);
 const PORT = 3000;
 mongoose
   .connect(
---
+++ backend/routes/user.js
@@ -0,0 +1,12 @@
+import express from "express";
+import {
+  getuser,
+  getuserfriends,
+  addRemoveFriends,
+} from "./controllers/user.js";
+import { verifytoken } from "../middleware/auth.js";
+const router = express.Router();
+router.get(":/id", verifytoken, getuser);
+router.get("/:id", verifytoken, getuserfriends);
+router.patch("/:id/:friendId", verifytoken, addRemoveFriends);
+export default Router;
