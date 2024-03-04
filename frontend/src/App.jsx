9d6d699c6ecc257670010ef04064c76077ed13c3
Manav Chawla <chawlamanav428@gmail.com>
2024-03-04 13:35:07 +0530

    redux setup ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,20 @@ import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/profile/:userId" element={<ProfilePage />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
